aeed1575f65d09961d30bddbc5bd58c1c7b45864
Sameer <hasansameer163@gmail.com>
2026-04-23 12:07:48 +0100

    Delete CW_title.py

--- CW_title.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-st.title("Freedom In World Dashboard")
-
-st.header("Exploring countries political rights and civil liberties globally")
-
-df = pd.read_csv("cleaned_freedom_dataset.csv")
-
